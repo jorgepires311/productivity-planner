3aad809d68371bee99e2ac2a40cf5b5e68c83f0d
Jorge Pires <jorgepires311@gmail.com>
2023-02-24 09:30:55 -0800

    Experimenting with API calls

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 const Quotes = () => {
-  const url = "https://zenquotes.io/api/quotes/";
-  const dailyQuote = async () => {
-    const res = await fetch(url, {
-      method: "POST", // Sending a POST request
-      mode: "cors",
-    });
-    const quoteData = await res.json();
-    return quoteData;
-  };
+  // const dailyQuote = async () => {
+  //   const url = "https://zenquotes.io/api/quotes/";
+  //   const options = {
+  //           mode: "no-cors"
+  //   };
+  //   const res = await fetch(url, options);
+  //   const quoteData = await res.json();
+  //   return quoteData;
+  // };
   useEffect(() => {
-    dailyQuote();
-  }, []);
+    fetch('https://zenquotes.io/api/quotes/').then(
+       (response) => console.log(response)
+    );
+ }, []);
+ 
+  const dailyQuote = fetch('https://zenquotes.io/api/quotes/')
+   .then(response => response.json())
+   .then(data => console.log(data));
+
+  // useEffect(() => {
+  //   dailyQuote();
+  // }, []);
+
   return (
     <div className="quoteContainer">
       <h2>Inspiration</h2>
-      {dailyQuote.h}
+      {/* {dailyQuote.h} */}
     </div>
   );
 };
